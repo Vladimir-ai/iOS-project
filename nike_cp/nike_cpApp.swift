07b85c7d12918e589d208899c57223c00b7e3f25
Vladimir Bespalov <bespalvovan42@gmail.com>
2023-11-25 15:31:22 +0300

    Initial Commit

---
+++ nike_cp/nike_cpApp.swift
@@ -0,0 +1,17 @@
+//
+//  nike_cpApp.swift
+//  nike_cp
+//
+//  Created by Adminisrator on 25.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct nike_cpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
