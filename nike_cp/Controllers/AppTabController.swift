2dd4308025bbf5d8ca096d91f7cb37f5f57e752c
Vladimir Bespalov <bespalvovan42@gmail.com>
2023-12-06 22:52:16 +0300

    [Project structure] Upgraded initial structure
    API - for interfaces only
    Resources - images, text
    Controllers - controllers
    View - Views
    ViewModel - models for views (maybe shared)

---
+++ nike_cp/Controllers/AppTabController.swift
@@ -0,0 +1,8 @@
+//
+//  TabController.swift
+//  nike_cp
+//
+//  Created by Adminisrator on 06.12.2023.
+//
+
+import Foundation
---
+++ nike_cp/TmpView.swift
@@ -0,0 +1,8 @@
+//
+//  TmpView.swift
+//  nike_cp
+//
+//  Created by Adminisrator on 06.12.2023.
+//
+
+import Foundation
