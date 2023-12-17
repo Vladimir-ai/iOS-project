3d5b54ae9cd67c88cb806fb25102b4c68d817aa2
Vladimir Bespalov <bespalvovan42@gmail.com>
2023-12-17 22:36:23 +0300

    [Shop][Common] Added Nike-style switch and gender selection in shop
    Common: Added Nike-style switch which allows to select one of possible values
    Shop: Added gender selection

--- nike_cp/Views/ShopView.swift
+++ nike_cp/Views/ShopView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ShopView : View {
+    static let spacing = 20.0
+    static let gender = [String(localized: "Men"), String(localized: "Women"), String(localized: "Kids")]
+
+    @StateObject var ShowMgr = ShopMgr()
+    
     var body: some View {
-        VStack {
-            Label("Shop view stub", systemImage: "bolt.fill").labelStyle(.titleOnly)
+        NavigationStack {
+            VStack(spacing: 0) {
+                SegmentedView(segments: ShopView.gender, selected: String(localized: "Men"))
+                Divider()
+                ScrollView
+                {
+                    
+                }
+            }
+            .navigationTitle(String(localized: "Shop"))
+            .toolbar {
+                Button(action: searchShop) {
+                    Label("Search", systemImage: "magnifyingglass")
+                        .labelStyle(.iconOnly)
+                }
+            }
         }
+    }
+    
+    func searchShop()
+    {
+        //TODO: Add search
+    }
+}
+
+struct ShopView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShopView()
     }
 }
---
+++ nike_cp/Views/ShopView/ShopMgr.swift
@@ -0,0 +1,14 @@
+//
+//  ShopMgr.swift
+//  nike_cp
+//
+//  Created by Adminisrator on 12.12.2023.
+//
+
+import Foundation
+
+
+class ShopMgr : ObservableObject
+{
+    
+}
