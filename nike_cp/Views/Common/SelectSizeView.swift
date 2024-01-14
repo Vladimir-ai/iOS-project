926ca17fb9559ff0d92254763a864e628e63f147
TNTrol <tntrol.go@gmail.com>
2024-01-14 10:24:47 -0800

    product: added link to select size

---
+++ nike_cp/Views/Common/SelectSizeView.swift
@@ -0,0 +1,17 @@
+//
+//  SelectSizeView.swift
+//  nike_cp
+//
+//  Created by tntrol on 14.01.2024.
+//
+
+import SwiftUI
+
+struct SelectSizeView: View {
+    
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
